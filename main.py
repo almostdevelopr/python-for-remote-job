79c216516b0be62194f6ec2e23fd059c53017b68
Minhaz Alam <minhalam.learn@gmail.com>
2023-05-20 21:27:48 +0530

    Add index

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input('Type add, show, edit or exit: ')
+    user_action = input('Type add, show, edit, complete or exit: ')
     user_action = user_action.strip()
 
     match user_action:
@@ -11,11 +11,14 @@ while True:
         case 'show':
             for index, item in enumerate(todos):
                 item = item.capitalize()
-                print(index, '-', item)
+                print(f'{index + 1} -{item}')
         case 'edit':
             existing_todo_index = int(input('Number of todo to edit: '))
             edited_todo = input('Edit your todo now: ')
             todos[existing_todo_index-1] = edited_todo
+        case 'complete':
+            number = int(input('Number of todo to complete: '))
+            todos.pop(number - 1)
         case 'exit':
             break
 print('Bye!')
